b8bc997f1ca151c930c595aeb01e5d268cd26489
juli-nvl <julianeneveling@gmail.com>
2025-04-06 16:58:00 +0200

    added realtime for all default cities

--- src/index.js
+++ src/index.js
@@ -5,6 +5,47 @@ function updateTime() {
   let parisTimezone = moment().tz("Europe / Paris");
   parisDate.innerHTML = parisTimezone.format("dddd, MMMM Do YYYY");
   parisTime.innerHTML = parisTimezone.format("hh:mm:ss [<small>]A [</small>]");
+
+  let newYork = document.querySelector("#new-york");
+  let newYorkDate = newYork.querySelector(".date");
+  let newYorkTime = newYork.querySelector(".time");
+  let newYorkTimezone = moment().tz("America/New_York");
+  newYorkDate.innerHTML = newYorkTimezone.format("dddd, MMMM Do YYYY");
+  newYorkTime.innerHTML = newYorkTimezone.format(
+    "hh:mm:ss [<small>]A [</small>]"
+  );
+
+  let saoPaulo = document.querySelector("#sao-paulo");
+  let saoPauloDate = saoPaulo.querySelector(".date");
+  let saoPauloTime = saoPaulo.querySelector(".time");
+  let saoPauloTimezone = moment().tz("America/Sao-Paulo");
+  saoPauloDate.innerHTML = saoPauloTimezone.format("dddd, MMMM Do YYYY");
+  saoPauloTime.innerHTML = saoPauloTimezone.format(
+    "hh:mm:ss [<small>]A [</small>]"
+  );
+
+  let tokyo = document.querySelector("#tokyo");
+  let tokyoDate = tokyo.querySelector(".date");
+  let tokyoTime = tokyo.querySelector(".time");
+  let tokyoTimezone = moment().tz("Asia/Tokyo");
+  tokyoDate.innerHTML = tokyoTimezone.format("dddd, MMMM Do YYYY");
+  tokyoTime.innerHTML = tokyoTimezone.format("hh:mm:ss [<small>]A [</small>]");
+
+  let lagos = document.querySelector("#lagos");
+  let lagosDate = lagos.querySelector(".date");
+  let lagosTime = lagos.querySelector(".time");
+  let lagosTimezone = moment().tz("Africa/Lagos");
+  lagosDate.innerHTML = lagosTimezone.format("dddd, MMMM Do YYYY");
+  lagosTime.innerHTML = lagosTimezone.format("hh:mm:ss [<small>]A [</small>]");
+
+  let sydney = document.querySelector("#sydney");
+  let sydneyDate = sydney.querySelector(".date");
+  let sydneyTime = sydney.querySelector(".time");
+  let sydneyTimezone = moment().tz("Australia/Sydney");
+  sydneyDate.innerHTML = sydneyTimezone.format("dddd, MMMM Do YYYY");
+  sydneyTime.innerHTML = sydneyTimezone.format(
+    "hh:mm:ss [<small>]A [</small>]"
+  );
 }
 
 setInterval(updateTime, 1000);
